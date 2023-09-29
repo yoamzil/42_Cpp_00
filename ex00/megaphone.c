bfcb2412540d3902202f81e1f708dd5a382f48c9
yoamzil <amzilyouness@gmail.com>
2023-09-29 18:46:35 +0100

    The first exercice is done... But in C, so time to switch to Cpp

--- ex00/megaphone.c
+++ ex00/megaphone.c
@@ -6,7 +6,7 @@
 /*   By: yoamzil <yoamzil@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 15:37:20 by yoamzil           #+#    #+#             */
-/*   Updated: 2023/09/29 15:56:36 by yoamzil          ###   ########.fr       */
+/*   Updated: 2023/09/29 18:40:42 by yoamzil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,21 +33,29 @@ void    starting_program(char *str)
     letter_capitalize(str);
     while (str[i])
     {
-        
+        if (str[i] != '"')
+            printf("%c", str[i]);
+        i++;
     }
 }
 int main(int ac, char **av)
 {
     int i;
+    int check;
 
     if (ac >= 2)
     {
-        i = 0;
-        while (i < ac -1)
+        i = 1;
+        check = 1;
+        while (i <= ac -1)
         {
+            if (!check)
+                printf(" ");
+            check = 0;
             starting_program(av[i]);
             i++;
         }
+        printf("\n");
     }
     else
         printf("* LOUD AND UNBEARABLE FEEDBACK NOISE *\n");
